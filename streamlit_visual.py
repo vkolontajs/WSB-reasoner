635b9d4cdfe9a8af566c60d328d8c739450b9fce
Vladislavs Kolontajs <74679731+vkolontajs@users.noreply.github.com>
2021-02-24 18:17:09 +0200

    Update streamlit_visual.py

--- streamlit_visual.py
+++ streamlit_visual.py
@@ -35,7 +35,7 @@ st.sidebar.text(sidebar_text)
 if st.sidebar.button('LinkedIn'):
     webbrowser.open_new_tab("https://www.linkedin.com/in/vladislavs-kolontajs-372293153")
 if st.sidebar.button('Project GitHub'):
-    webbrowser.open_new_tab("https://github.com/vkolontajs")
+    webbrowser.open_new_tab("https://github.com/vkolontajs/WSB-reasoner")
 if st.sidebar.button('Buy a coffee for my bot (crypto)'):
     webbrowser.open_new_tab("https://commerce.coinbase.com/checkout/483140a7-ae94-42e7-9bfa-6119365f44ea")
 
